d9c58b900faabb010265afc254dcffdf86fba732
Sherwyn Ng <>
2025-05-26 00:34:02 +0800

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,36 @@
+using Core.Interfaces;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddDbContext<StoreContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try 
+    {
+        var context = services.GetRequiredService<StoreContext>();
+        await context.Database.MigrateAsync();
+        await StoreContextSeed.SeedAsync(context, loggerFactory);
+    }
+    catch (Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "An error occured during database migration");
+    }
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -41,20 +61,36 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
-app.MapGet("/api/products", async (StoreContext context) =>
+app.MapGet("/api/products", async (IProductRepository repo) =>
 {
-    var products = await context.Products.ToListAsync();
+    var products = await repo.GetProductsAsync();
     return Results.Ok(products);
 })
 .WithName("GetProducts")
 .WithOpenApi();
 
-app.MapGet("/api/products/{id}", async (int id, StoreContext context) =>
+app.MapGet("/api/products/{id}", async (int id, IProductRepository repo) =>
 {
-    var product = await context.Products.FindAsync(id);
+    var product = await repo.GetProductByIdAsync(id);
     return product is not null ? Results.Ok(product) : Results.NotFound();
 })
 .WithName("GetProductsById")
+.WithOpenApi();
+
+app.MapGet("/api/products/brands", async (IProductRepository repo) =>
+{
+    var brands = await repo.GetProductBrandsAsync();
+    return Results.Ok(brands);
+})
+.WithName("GetProductBrands")
+.WithOpenApi();
+
+app.MapGet("/api/products/types", async (IProductRepository repo) =>
+{
+    var brands = await repo.GetproductTypesAsync();
+    return Results.Ok(brands);
+})
+.WithName("GetProductTypes")
 .WithOpenApi();
 
 app.Run();
